28f2c31267dc3fa5a4f01842f37962ab9824346b
Bogdan <bpintilei@gmail.com>
2018-02-20 11:51:14 +0200

    Project and server setup

---
+++ CarsApp-MobileExam/CarsApp-MobileExam/Models/Object.swift
@@ -0,0 +1,36 @@
+//
+//  Object.swift
+//  CarsApp-MobileExam
+//
+//  Created by Bogdan Pintilei on 2/20/18.
+//  Copyright © 2018 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class Object: NSObject {
+
+    var id: Int?
+    var name: String?
+    var model: String?
+    var status: String?
+    var km: Int?
+    var year: Int?
+
+    init(
+        id: Int? = 0,
+        name: String? = "",
+        model: String? = "",
+        status: String? = "",
+        km: Int? = 0,
+        year: Int = 0
+    ) {
+        self.id = id
+        self.name = name
+        self.model = model
+        self.status = status
+        self.km = km
+        self.year = year
+    }
+    
+}
---
+++ CarsApp-MobileExam/CarsApp-MobileExam/View Controllers/CustomTableViewCell/CustomTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomTableViewCell.swift
+//  CarsApp-MobileExam
+//
+//  Created by Bogdan Pintilei on 2/20/18.
+//  Copyright © 2018 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var objectNameLabel: UILabel!
+    @IBOutlet var objectIDLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
